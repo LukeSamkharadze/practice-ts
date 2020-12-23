62107db5363bff3b8095f1cf728280781acf2a09
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-12-23 18:59:52 +0400

    add reflect-metadata

--- 5-decorators-and-mixins/1-lecture/decorators/5.parameter decorator/example-11.ts
+++ 5-decorators-and-mixins/1-lecture/decorators/5.parameter decorator/example-11.ts
@@ -2,62 +2,38 @@ import "reflect-metadata";
 
 const requiredMetadataKey = Symbol("required");
 {
+  class Greeter {
+    greeting: string;
 
-
-    class Greeter {
-        greeting: string;
-
-        constructor(message: string) {
-            this.greeting = message;
-        }
-
-        @validate
-        greet(@required name: string) {
-            return "Hello " + name + ", " + this.greeting;
-        }
+    constructor(message: string) {
+      this.greeting = message;
     }
 
-
-    function required(
-        target: Object,
-        propertyKey: string | symbol,
-        parameterIndex: number
-    ) {
-        let existingRequiredParameters: number[] =
-            Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
-        existingRequiredParameters.push(parameterIndex);
-        Reflect.defineMetadata(
-            requiredMetadataKey,
-            existingRequiredParameters,
-            target,
-            propertyKey
-        );
+    @validate
+    greet(@required name: string) {
+      return "Hello " + name + ", " + this.greeting;
     }
+  }
 
-    function validate(
-        target: any,
-        propertyName: string,
-        descriptor: TypedPropertyDescriptor<Function>
-    ) {
-        let method = descriptor.value;
-        descriptor.value = function () {
-            let requiredParameters: number[] = Reflect.getOwnMetadata(
-                requiredMetadataKey,
-                target,
-                propertyName
-            );
-            if (requiredParameters) {
-                for (let parameterIndex of requiredParameters) {
-                    if (
-                        parameterIndex >= arguments.length ||
-                        arguments[parameterIndex] === undefined
-                    ) {
-                        throw new Error("Missing required argument.");
-                    }
-                }
-            }
 
-            return method.apply(this, arguments);
-        };
-    }
+  function required(target: Object, propertyKey: string | symbol, parameterIndex: number) {
+    let existingRequiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+    existingRequiredParameters.push(parameterIndex);
+    Reflect.defineMetadata(requiredMetadataKey, existingRequiredParameters, target, propertyKey);
+  }
+
+  function validate(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) {
+    let method = descriptor.value;
+
+    descriptor.value = function () {
+      let requiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName);
+
+      if (requiredParameters)
+        for (let parameterIndex of requiredParameters)
+          if (parameterIndex >= arguments.length || arguments[parameterIndex] === undefined)
+            throw new Error("Missing required argument.");
+
+      return method.apply(this, arguments);
+    };
+  }
 }
